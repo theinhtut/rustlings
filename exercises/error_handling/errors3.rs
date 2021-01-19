64a308f038b8ee99f2bc50926a43eee8db20a74a
Thein Htut <theinhtut.dec@gmail.com>
2021-01-19 12:53:27 +0800

    Solved errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,21 +4,41 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+// #1 Using match pattern
+// fn main() {
+//     let mut tokens = 100;
+//     let pretend_user_input = "8";
+
+//     match total_cost(pretend_user_input) {
+//         Ok(cost) => {
+//             if cost > tokens {
+//                 println!("You can't afford that many!");
+//             } else {
+//                 tokens -= cost;
+//                 println!("You now have {} tokens.", tokens);
+//             }
+//         },
+//         Err(error) => {
+//             println!("Error {:?}", error);
+//         }
+//     };
+// }
+
+// #2 Chaning main() return type
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
 
     if cost > tokens {
-        println!("You can't afford that many!");
+        Ok(println!("You can't afford that many!"))
     } else {
         tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+        Ok(println!("You now have {} tokens.", tokens))
     }
 }
 
